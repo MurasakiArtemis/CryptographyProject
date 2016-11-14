d439cba02a0f41845d539edc6b4d7dd4f07485dd
becusyanke <becusyanke@gmail.com>
2016-11-13 20:42:43 -0600

    DES

--- src/cryptography/TestDES.java
+++ src/cryptography/TestDES.java
@@ -1,5 +1,7 @@
 package cryptography;
 
+import java.util.Random;
+
 //Example using DES class
 
 public class TestDES 
@@ -9,25 +11,25 @@ public class TestDES
 	{
 	DES des=new DES();
 	
-	//Generate a key
-	char[] key=des.generate_key();
-	
-	//Generate key_set
-	key_set[] key_sets=des.generate_sub_keys(key);
-	
 	char[] message={'h', 'o', 'l', 'a', 'e', 'r', 'w', 'n'};
 	
-		for (int i=0; i<8; i++) System.out.print(""+message[i]);
-		System.out.println();
+	for (int i=0; i<8; i++) System.out.print(""+message[i]);
+	System.out.println();
+	
+	char[] key=new char[8];
+	Random r=new Random();
+	
+		for (int i=0; i<7; i++) key[i] = (char)r.nextInt(255);
+	
 	
 	//Encrypt block of 8 bytes
-	char[] enc=des.process_message(message, key_sets, 1);
+	char[] enc=des.process_message(message, key, 1);
 	
 		for (int i=0; i<8; i++) System.out.print(""+enc[i]);
 		System.out.println();
 		
 	//Decrypt block of 8 bytes
-	char[] dec=des.process_message(enc, key_sets, 0);
+	char[] dec=des.process_message(enc, key, 0);
 		
 		for (int i=0; i<8; i++) System.out.print(""+dec[i]);
 		System.out.println();
