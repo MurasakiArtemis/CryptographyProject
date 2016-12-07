8c3bb55da0e5bbb04440ab57e5bde8c79b6f353e
MurasakiArtemis <erwin.hernandezg@gmail.com>
2016-12-07 17:27:27 -0600

    Completed the test/example

--- src/cryptography/TestWn.java
+++ src/cryptography/TestWn.java
@@ -3,31 +3,47 @@ package cryptography;
 import java.io.File;
 /*import java.security.SecureRandom;
 import java.util.Random;*/
+import java.util.Random;
 
 public class TestWn {
 
 	public static void main(String[] args) throws Exception 
 	{
-		if(args.length != 5)
+		if(args.length != 6)
 		{
-			System.out.println("Modo de uso: <Algoritmo> <Modo de operacion> <Archivo.extension> <Llave> <-d|-e>");
+			System.out.println("Modo de uso: <Algoritmo> <Modo de operacion> <Archivo.extension> <Llave> <-d|-e> <Nombre destino>");
 			return;
+		}
+		KeyRing key_ring = new KeyRing(new File("C:\\Users\\erwin\\Desktop\\keyring.key"));
+		if(key_ring.key_ring.isEmpty())
+		{
+			Random rand = new Random();
+			Curvas key_generator = new Curvas();
+			CurvesKey[] keys = key_generator.generateKeyPair();
+			CurvesKey public_key = keys[1], private_key = keys[0];
+			key_ring.add_key_pair(public_key, private_key);
+			key_ring.add_public_key("Manuel", key_generator.generateKeyPair()[1]);
+			key_ring.add_public_key("Juan", key_generator.generateKeyPair()[1]);
+			key_ring.add_public_key("Enrique", key_generator.generateKeyPair()[1]);
+			key_ring.add_public_key("Mariana", key_generator.generateKeyPair()[1]);
+			key_ring.add_public_key("Artemis", key_generator.generateKeyPair()[1]);
 		}
 		Algorithm algorithm = Algorithm.valueOf(args[0]);
 		OperationModes operation_mode = OperationModes.valueOf(args[1]);
 		String[] filename = args[2].split("\\.");
 		File keyfile = new File(args[3]);
 		boolean encrypt = args[4].compareToIgnoreCase("-e") == 0;
+		String nombre_destinatario = args[5];
 		File origin_file = new File(filename[0] + "." + filename[1]);
 		if(encrypt)
 		{
-			CipherDecipher cipher = new CipherDecipher(keyfile, algorithm);
+			CipherDecipher cipher = new CipherDecipher(keyfile, algorithm, nombre_destinatario, key_ring);
 			File destination_file = new File(filename[0] + "_" + operation_mode + "_" + algorithm + "_Cipher." + filename[1]);
 			cipher.encipher(origin_file, destination_file, operation_mode);
 		}
 		else
 		{
-			CipherDecipher decipher = new CipherDecipher(keyfile);
+			CipherDecipher decipher = new CipherDecipher(keyfile, key_ring);
 			File recovered_file = new File(filename[0] + "_" + operation_mode + "_" + algorithm + "_Decipher." + filename[1]);
 			decipher.decipher(origin_file, recovered_file);
 		}
